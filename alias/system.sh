cc359c8663d9085fa87f40ae026008d780c36230
Jim Fitzpatrick <jimfity@gmail.com>
2019-10-11 18:18:04 +0100

    Update to config files

---
+++ alias/system.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+alias dirs="dirs -v"
+alias quit="exit"
+
+alias open="xdg-open"
+alias size="du -ch , | grep total"
---
+++ source.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source ~/code/github.com/boomatang/dotfiles/alias/git
+source ~/code/github.com/boomatang/dotfiles/alias/system.sh
+source ~/code/github.com/boomatang/dotfiles/config/display.sh
