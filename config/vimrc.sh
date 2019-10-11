1273d19b69d2a4546ef7f002a72224d875a1b4a8
Jim Fitzpatrick <jimfity@gmail.com>
2019-10-11 19:11:12 +0100

    Vim Config file added with basic setting

---
+++ config/vimrc.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+colo delek
+
+filetype plugin indent on
+syntax on
+
+set showmode
+
+
+set autoindent
+set tabstop=2
+set shiftwidth=2
+set expandtab
+
+
+set number
+set relativenumber
+
+" These work as expected when you are not on the last line of the page "
+map #2 :set number <enter> :set relativenumber <enter>
+map #3 :set nonumber <enter> :set norelativenumber <enter>
