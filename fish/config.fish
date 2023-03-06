3f8998a0cacde8f056d02710ab2bc1dd4013fd99
Jim Fitzpatrick <jfitzpat@redhat.com>
2023-03-06 13:25:52 +0000

    Setup required paths

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,18 @@
 if status is-interactive
     set EDITOR /usr/bin/vim
     set FISH_USER_CLUSTER_CHECK 1
+    
     set DOTFILES $HOME/code/github.com/Boomatang/dotfiles
+    set DOT_SCRIPTS $DOTFILES/scripts
+
+    set GOPATH $HOME/go
+    set GOSRC $GOPATH/src
+    set PATH $PATH /usr/bin/go/bin $GOPATH/bin
+    set PATH $PATH $HOME/bin $HOME/.poetry/bin
+    set PATH $PATH $HOME/.local/bin
+
+    set KUBE_EDITOR vim
+
     alias k kubectl
     # Commands to run in interactive sessions can go here
 end
